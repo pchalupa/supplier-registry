1a2dfcd6f3c0e7275d8f990422acc910a5b2f0ce
pchalupa <chalupa.petr93@gmail.com>
2024-05-26 10:15:26 +0200

    chore: first commit

---
+++ src/main/java/com/supplierregistry/supplier_registry/SupplierRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.supplierregistry.supplier_registry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupplierRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupplierRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supplierregistry/supplier_registry/SupplierRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supplierregistry.supplier_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplierRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
