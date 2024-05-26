b12991b75482a31275a30a4923ecffc0483057b6
pchalupa <chalupa.petr93@gmail.com>
2024-05-26 14:49:38 +0200

    feat: add healthcheck endpoint

--- src/main/java/com/supplierregistry/supplierregistry/SupplierRegistryApplication.java
+++ src/main/java/com/supplierregistry/supplierregistry/SupplierRegistryApplication.java
@@ -3,16 +3,10 @@ package com.supplierregistry.supplierregistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// import com.supplierregistry.supplierregistry.providers.*;
-// import com.supplierregistry.supplierregistry.providers.Ares.AresProvider;
-
 @SpringBootApplication
 public class SupplierRegistryApplication {
 
 	public static void main(String[] args) {
-		// AresProvider ares = new AresProvider();
-
-		// ares.findByIco("06149341");
 		SpringApplication.run(SupplierRegistryApplication.class, args);
 	}
 
