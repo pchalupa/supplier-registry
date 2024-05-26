c8de192793f6578e7db663473e271ad63f7b0ba3
Matouš Janeček <73238324+feidar@users.noreply.github.com>
2024-05-26 15:49:54 +0200

    Update BaseResponse.java

--- src/main/java/com/supplierregistry/supplierregistry/rest/model/BaseResponse.java
+++ src/main/java/com/supplierregistry/supplierregistry/rest/model/BaseResponse.java
@@ -1,32 +1,6 @@
 package com.supplierregistry.supplierregistry.rest.model;
 
 public class BaseResponse<T> {
-//    private boolean success;
-//    private String message;
-//
-//    public BaseResponse() {}
-//
-//    public BaseResponse(boolean success, String message) {
-//        this.success = success;
-//        this.message = message;
-//    }
-//
-//    // Gettery a settery
-//    public boolean isSuccess() {
-//        return success;
-//    }
-//
-//    public void setSuccess(boolean success) {
-//        this.success = success;
-//    }
-//
-//    public String getMessage() {
-//        return message;
-//    }
-//
-//    public void setMessage(String message) {
-//        this.message = message;
-//    }
 private boolean success;
     private T data;
 
